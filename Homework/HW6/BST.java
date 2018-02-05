9df55acac5ddb9bace04d33cf41940ac081ac032
Junrong <Junrongh@Junrongs-Macbook-Air.local>
2018-02-05 18:42:23 -0500

    Word class finished
    Word class as a data type, will be used in the later BST, described as
    <T> in BST.java

--- Homework/HW6/BST.java
+++ Homework/HW6/BST.java
@@ -1,14 +1,74 @@
+/**
+ * 08722 Data Structures for Application Programmers.
+ *
+ * Homework 6. BST class
+ * @author Junrong Huang
+ */
+
 import java.util.Comparator;
 import java.util.Iterator;
 
+/**
+* @param T: any type of data
+*/
 public class BST<T extends Comparable<T>> implements Iterable<T>, BSTInterface<T> {
+    
+    // Node: private static nested class
+    private static class Node<T> {
+        private T data;
+        private Node<T> left;
+        private Node<T> right;
+
+        /**
+         * Constructor without left, right children
+         * @param d the data of the new node.
+         */
+        public Node(T d) {
+            this.data = d;
+            this.left = null;
+            this.right = null;
+        }
+
+        /**
+         * Constructor with left, right children
+         * @param d the data of the new node.
+         * @param l left node.
+         * @param r right node.
+         */
+        public Node(T d, Node<T> l, Node<T> r) {
+            this.data = d;
+            this.left = l;
+            this.right = r;
+        }
+
+        /**
+         * Convert the current node to String.
+         * @return string representation of current node.
+         */
+        @Override
+        public String toString() {
+            return data.toString();
+        }
+
+    }
+
+
     private Node<T> root;
     private Comparator<T> comparator;
 
+    /**
+     * Constructor with no parameter
+     */
     public BST() {
         this(null);
+        // this.root = null;
     }
 
+    /**
+     * Constructor with a comparator
+     * 
+     * @param comparator: the comparator to compare T
+     */
     public BST(Comparator<T> comp) {
         comparator = comp;
         root = null;
@@ -20,6 +80,10 @@ public class BST<T extends Comparable<T>> implements Iterable<T>, BSTInterface<T
 
     public T getRoot() {
         // TODO implement this
+        if (root == null) {
+            return null;
+        }
+        return root.data;
     }
 
     public int getHeight() {
--- Homework/HW6/Word.java
+++ Homework/HW6/Word.java
@@ -1,4 +1,12 @@
+/**
+ * 08722 Data Structures for Application Programmers.
+ *
+ * Homework 6. Word class
+ * @author Junrong Huang
+ */
+
 import java.util.Set;
+import java.util.HashSet;
 
 public class Word implements Comparable<Word> {
     private String word;
@@ -6,4 +14,128 @@ public class Word implements Comparable<Word> {
     private int frequency;
 
     // TODO implement methods below.
+
+    /**
+     * Constructor
+     * @param wd: the input word of String
+     */
+    public Word(String wd) {
+        if (wd == null || wd.length() == 0 || !wd.matches("[a-zA-Z]+")) {
+            return;
+        }
+        this.word = wd;
+        this.index = new HashSet<Integer>();
+        this.frequency = 1;
+    }
+
+    /**
+     * Constructor
+     * @param wd: the input word of String
+     * @param indexSet: the set contains all index of this word
+     * @param freq: the frequency wo this word
+     */
+    public Word(String wd, Set<Integer> indexSet, int freq) {
+        if (wd == null || wd.length() == 0 || !wd.matches("[a-zA-Z]+")) {
+            return;
+        }
+        this.word = wd;
+        this.index = new HashSet<Integer>(indexSet);
+        this.frequency = freq;
+    }
+
+
+    /**
+     * Get the word in Word.
+     * @return the word in the Word class.
+     */
+    public String getWord() {
+        return this.word;
+    }
+
+   /**
+     * Get the index Set of Word class.
+     * @return the index Set of Word class.
+     */
+    public Set<Integer> getIndex() {
+        return this.index;
+    }
+
+    /**
+     * Get the frequency of Word class.
+     * @return the frequency of Word class.
+     */
+    public int getFrequency() {
+        return this.frequency;
+    }
+
+    /**
+     * Set the word with str.
+     * @param str the word to be used in the new node.
+     */
+    public void setWord(String str) {
+        if (word == null || word.length() == 0 || !word.matches("[a-zA-Z]+")) {
+            return;
+        }
+        this.word = str;
+    }
+
+    /**
+     * Set the indexSet with new set.
+     * @param set the set to replace the indexSet.
+     */
+    public void setIndex(Set<Integer> set) {
+        this.index = new HashSet<Integer>(set);
+    }
+
+    /**
+     * Set the frequency of the Word Class.
+     * @param freq the frequency of the Word Class.
+     */
+    public void setFrequency(int freq) {
+        int res = freq;
+        this.frequency = res;
+    }
+
+    /**
+     * @param line: the new line number for the word
+     *              that should be added to the index
+     */
+    public void addToIndex(Integer line) {
+        this.index.add(line);
+    }
+
+    /**
+     * @return String, words and its descriptors
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        String space = " ";
+        sb.append(this.word);
+        sb.append(space);
+        sb.append(this.frequency);
+        sb.append(space);
+        sb.append("index : [");
+        int i = 0;
+        for (Integer num : index) {
+            sb.append(num);
+            if (i != index.size() - 1) {
+                sb.append(", ");
+            }
+            i++;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    /**
+     * compareTo method
+     * @param wd: word to be compared
+     * @return comparison result
+     */
+    @Override
+    public int compareTo(Word wd) {
+        return this.word.compareTo(wd.word);
+    }
+
 }
